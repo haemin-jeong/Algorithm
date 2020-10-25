4371356010d4a83493194e6f6cded605a72e1df6
Haemin Jeong <jhm0828@gmail.com>
2020-10-25 20:59:44 +0900

    Add boj_11021

---
+++ Math/boj_11021.java
@@ -0,0 +1,22 @@
+//Date : 2020-10-25
+//Link : https://www.acmicpc.net/problem/11021
+package Math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_11021 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            System.out.printf("Case #%d: %d\n", i, a + b);
+        }
+    }
+}
