8b0a247f37713f24a0bcc6b41b675b0df88c1f50
Haemin Jeong <jhm0828@gmail.com>
2020-11-05 13:17:43 +0900

    Add 프로그래머스 : K번째수

---
+++ Programmers/K번째수.java
@@ -0,0 +1,24 @@
+// 날짜 : 2020-11-5
+// 출처 : https://programmers.co.kr/learn/courses/30/lessons/42748
+
+package Programmers;
+
+import java.util.*;
+
+public class K번째수 {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for (int i = 0; i < commands.length; i++) {
+            int arr[] = new int[commands[i][1] - commands[i][0] + 1];
+
+            System.arraycopy(array, commands[i][0] - 1, arr, 0, arr.length);
+
+            Arrays.sort(arr);
+
+            answer[i] = arr[commands[i][2] - 1];
+        }
+
+        return answer;
+    }
+}
