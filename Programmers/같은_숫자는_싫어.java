6aa2147d065462532fce19ecaecbbf831606a56d
Haemin Jeong <jhm0828@gmail.com>
2020-11-10 20:15:46 +0900

    Add 프로그래머스 : 같은 숫자는 싫어

---
+++ Programmers/같은_숫자는_싫어.java
@@ -0,0 +1,26 @@
+// 날짜 : 2020-11-10
+//출처 : https://programmers.co.kr/learn/courses/30/lessons/12906
+
+package Programmers;
+
+import java.util.*;
+
+public class 같은_숫자는_싫어 {
+    public int[] solution(int[] arr) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        int prev = arr[0];
+        list.add(arr[0]);
+
+        for (int i = 1; i < arr.length; i++) {
+            if (prev == arr[i]) {
+                continue;
+            }
+
+            list.add(arr[i]);
+            prev = arr[i];
+        }
+
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
