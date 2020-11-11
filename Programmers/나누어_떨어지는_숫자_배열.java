10e23630393c9802b5d3f49b5ac2f6c2fe9c50c6
Haemin Jeong <jhm0828@gmail.com>
2020-11-11 22:34:26 +0900

    Add 프로그래머스 : 나누어 떨어지는 숫자 배열

---
+++ Programmers/나누어_떨어지는_숫자_배열.java
@@ -0,0 +1,24 @@
+// 날짜 : 2020-11-11
+// 출처 : https://programmers.co.kr/learn/courses/30/lessons/12910
+
+package Programmers;
+
+import java.util.*;
+
+public class 나누어_떨어지는_숫자_배열 {
+    public int[] solution(int[] arr, int divisor) {
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % divisor == 0) {
+                list.add(arr[i]);
+            }
+        }
+
+        if (list.size() == 0) {
+            return new int[] { -1 };
+        }
+
+        return list.stream().sorted().mapToInt(Integer::intValue).toArray();
+    }
+}
