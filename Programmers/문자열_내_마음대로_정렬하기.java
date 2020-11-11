fa0c5cebd906be18733a63fd82411b80b65735ee
Haemin Jeong <jhm0828@gmail.com>
2020-11-12 00:56:46 +0900

    Add 프로그래머스 : 문자열 내 마음대로 정렬하기

---
+++ Programmers/문자열_내_마음대로_정렬하기.java
@@ -0,0 +1,20 @@
+// 날짜 : 2020-11-12
+// 출처 : https://programmers.co.kr/learn/courses/30/lessons/12915
+
+package Programmers;
+
+import java.util.*;
+
+public class 문자열_내_마음대로_정렬하기 {
+    public String[] solution(String[] strings, int n) {
+        Arrays.sort(strings, (s1, s2) -> {
+            if (s1.charAt(n) == s2.charAt(n)) {
+                return s1.compareTo(s2);
+            }
+
+            return s1.charAt(n) - s2.charAt(n);
+        });
+
+        return strings;
+    }
+}
