f291bd8991257350d8ae84690b8986c98939bd4b
Haemin Jeong <jhm0828@gmail.com>
2020-11-12 20:03:28 +0900

    Add 프로그래머스 : 문자열 내림차순으로 배치하기

---
+++ Programmers/문자열_내림차순으로_배치하기.java
@@ -0,0 +1,20 @@
+// 날짜 : 2020-11-12
+// 출처 : https://programmers.co.kr/learn/courses/30/lessons/12917
+
+package Programmers;
+
+import java.util.*;
+
+public class 문자열_내림차순으로_배치하기 {
+    public String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+        char[] arr = s.toCharArray();
+        Arrays.sort(arr);
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            sb.append(arr[i]);
+        }
+
+        return sb.toString();
+    }
+}
