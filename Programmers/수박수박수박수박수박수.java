e823e2fce4040ebef312e8ae88c54423e5b8e3b1
Haemin Jeong <jhm0828@gmail.com>
2020-11-12 21:24:26 +0900

    Add 프로그래머스 : 수박수박수박..

---
+++ Programmers/수박수박수박수박수박수.java
@@ -0,0 +1,21 @@
+// 날짜 : 2020-11-12
+// 출처 : https://programmers.co.kr/learn/courses/30/lessons/12922
+
+package Programmers;
+
+public class 수박수박수박수박수박수 {
+    public String solution(int n) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                sb.append("수");
+            } else {
+                sb.append("박");
+            }
+        }
+
+        return sb.toString();
+    }
+
+}
