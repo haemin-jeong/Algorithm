5675be22e5fa07df35a838b53378196ab3a6bc1f
haemin-jeong <39875618+haemin-jeong@users.noreply.github.com>
2020-11-13 21:11:42 +0900

    Add 프로그래머스 : 시저 암호

---
+++ Programmers/시저암호.java
@@ -0,0 +1,34 @@
+package Programmers;
+
+public class 시저암호 {
+    public String solution(String s, int n) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 0; i < s.length(); i++) {
+            //공백일때 
+            if(s.charAt(i) == ' ') {
+                sb.append(" ");
+                continue;
+            }
+            
+            //소문자일때
+            if(Character.isLowerCase(s.charAt(i))) {
+                if(s.charAt(i)+n > 122) {
+                    sb.append((char)(s.charAt(i) + n - 122 + 96));
+                } else {
+                    sb.append((char)(s.charAt(i) + n));    
+                }
+                continue;
+            }
+            
+            //대문자일때
+            if(s.charAt(i) + n > 90) {
+                sb.append((char)(s.charAt(i) + n - 90 + 64));
+            } else {
+                sb.append((char)(s.charAt(i) + n));
+            }
+        }
+        
+        return sb.toString();
+    }
+}
