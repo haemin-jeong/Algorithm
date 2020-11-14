3df050c6d6994f326451ff7a83a8aff193487149
haemin-jeong <39875618+haemin-jeong@users.noreply.github.com>
2020-11-14 23:42:26 +0900

    Add 프로그래머스 : 정수 제곱근 판별

---
+++ Programmers/정수_제곱근_판별.java
@@ -0,0 +1,15 @@
+// 풀이 날짜 : 2020-11-14
+// 문제 출처 : https://programmers.co.kr/learn/courses/30/lessons/12934
+package Programmers;
+
+public class 정수_제곱근_판별 {
+    public long solution(long n) {
+        double x = Math.sqrt(n);
+
+        if((int)x != x) {
+            return -1;
+        } 
+        
+        return (long)((x +1)*(x +1));
+    }
+}
