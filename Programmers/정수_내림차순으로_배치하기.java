2cf61c9cd6c772463d6beb37f1c8088936748dd0
haemin-jeong <39875618+haemin-jeong@users.noreply.github.com>
2020-11-14 23:00:42 +0900

    Add 프로그래머스 : 정수 내림차순으로 배치하기

---
+++ Programmers/정수_내림차순으로_배치하기.java
@@ -0,0 +1,21 @@
+// 풀이 날짜 : 2020-11-14
+// 문제 출처 : https://programmers.co.kr/learn/courses/30/lessons/12933
+
+package Programmers;
+
+import java.util.*;
+
+public class 정수_내림차순으로_배치하기 {
+    public long solution(long n) {
+        StringBuilder sb = new StringBuilder();
+        char[] arr = Long.toString(n).toCharArray();
+        
+        Arrays.sort(arr);
+        
+        for(int i = arr.length-1; i >= 0; i--) {
+            sb.append(arr[i]);
+        }
+        
+        return Long.parseLong(sb.toString());
+    }
+}
