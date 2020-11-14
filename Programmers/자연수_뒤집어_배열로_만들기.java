34506329ab18c1c92ff728d245f5d9ce0c143ca5
haemin-jeong <39875618+haemin-jeong@users.noreply.github.com>
2020-11-14 17:08:12 +0900

    Add 프로그래머스 : 자연수 뒤집어 배열로 만들기

---
+++ Programmers/자연수_뒤집어_배열로_만들기.java
@@ -0,0 +1,18 @@
+// 날짜 : 2020-11-14
+// 출처 : https://programmers.co.kr/learn/courses/30/lessons/12932
+
+package Programmers;
+
+public class 자연수_뒤집어_배열로_만들기 {
+    public int[] solution(long n) {
+        String s = Long.toString(n);
+        int[] answer = new int[s.length()];
+        int index = 0;
+        
+        for(int i = s.length()-1; i >= 0; i--) {
+            answer[index++] = s.charAt(i) - '0';
+        }
+        
+        return answer;
+    }
+}
