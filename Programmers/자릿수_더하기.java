41271ca092e77e19c07e93dc02556294ee7d4bc6
haemin-jeong <39875618+haemin-jeong@users.noreply.github.com>
2020-11-14 16:45:27 +0900

    Add 프로그래머스 : 자릿수 더하기

---
+++ Programmers/자릿수_더하기.java
@@ -0,0 +1,17 @@
+// 날짜 : 2020-11-14
+// 출처 : https://programmers.co.kr/learn/courses/30/lessons/12931
+
+package Programmers;
+
+public class 자릿수_더하기 {
+    public int solution(int n) {
+        int sum = 0;
+        String s = Integer.toString(n);
+        
+        for(int i = 0; i < s.length(); i++) {
+            sum += s.charAt(i) - '0';
+        }
+        
+        return sum;
+    }
+}
