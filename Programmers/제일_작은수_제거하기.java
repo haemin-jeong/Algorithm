2bb753f96aa60a0aac23e84efb7fce7c32a24b1c
haemin-jeong <39875618+haemin-jeong@users.noreply.github.com>
2020-11-15 00:30:08 +0900

    Add 프로그래머스 : 제일 작은 수 제거하기

---
+++ Programmers/제일_작은수_제거하기.java
@@ -0,0 +1,19 @@
+
+
+package Programmers;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class 제일_작은수_제거하기 {
+    public int[] solution(int[] arr) {
+        if(arr.length == 1) {
+            return new int[]{-1};
+        }
+        
+        List<Integer> list = new ArrayList<>(Arrays.stream(arr).boxed().collect(Collectors.toList()));
+        list.remove(list.remove(Collections.min(list)););
+        
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
