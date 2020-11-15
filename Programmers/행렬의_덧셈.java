e4ca4f87bcb2b2b86dc40893f42245d967cb20ad
haemin-jeong <39875618+haemin-jeong@users.noreply.github.com>
2020-11-15 20:59:53 +0900

    Add 프로그래머스 : 행렬의 덧셈

---
+++ Programmers/행렬의_덧셈.java
@@ -0,0 +1,19 @@
+// 풀이 날짜 : 2020-11-15
+// 문제 출처 : https://programmers.co.kr/learn/courses/30/lessons/12950
+
+package Programmers;
+
+public class 행렬의_덧셈 {
+
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        
+        for(int i = 0; i < arr1.length; i++) {
+            for(int j = 0; j < arr1[0].length; j++) {
+                arr1[i][j] += arr2[i][j];
+            }
+        }
+        
+        return arr1;
+    }
+    
+}
