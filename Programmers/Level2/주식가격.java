767a169c1f6e02758131d752cf142ac7a6a3b544
haemin-jeong <39875618+haemin-jeong@users.noreply.github.com>
2020-11-24 13:50:55 +0900

    Add 프로그래머스 : 주식가격

---
+++ Programmers/Level2/주식가격.java
@@ -0,0 +1,28 @@
+// 풀이 날짜 : 2020-11-24
+// 문제 출처 : https://programmers.co.kr/learn/courses/30/lessons/42584
+
+package Programmers.Level2;
+
+public class 주식가격 {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        
+        for(int i = 0 ; i < prices.length; i++) {
+            
+            int second = 0;
+            for(int j = i+1; j < prices.length; j++) {
+
+                second++;
+                
+                if(prices[i] > prices[j]) {
+                    break;
+                }
+                
+                
+            }
+            answer[i] = second;
+        }
+        
+        return answer;
+    }
+}
