825f3baeb471ade7fa8c67855beb2eed9d79050a
Haemin Jeong <jhm0828@gmail.com>
2021-05-31 22:15:32 +0900

    Create Main4_5.java

---
+++ inflearn/hashmap_treeset/Main4_5.java
@@ -0,0 +1,53 @@
+package inflearn.hashmap_treeset;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+//K번째 큰 수
+public class Main4_5 {
+
+    public static int solution(int n, int k, int[] arr) {
+        TreeSet<Integer> set = new TreeSet<>(Collections.reverseOrder()); //중복 제거, 정렬
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int l = j + 1; l < n; l++) {
+                    set.add(arr[i] + arr[j] + arr[l]);
+                }
+            }
+        }
+
+        int count = 1;
+        for (Integer num : set) {
+
+            if (count == k) {
+                return num;
+            }
+
+            count++;
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; st.hasMoreTokens(); i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(solution(n, k, arr));
+    }
+}
