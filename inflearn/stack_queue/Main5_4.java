62a046e66f7dd6500e7305e873c4d8c56f84c913
Haemin Jeong <jhm0828@gmail.com>
2021-06-02 19:10:01 +0900

    Create Main5_4.java

---
+++ inflearn/stack_queue/Main5_4.java
@@ -0,0 +1,42 @@
+package inflearn.stack_queue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+//후위식 연산(postfix)
+public class Main5_4 {
+
+    //숫자면 스택에 넣고, 연산자면 스택에서 두개를 꺼내서 연산하고 연산 결과를 스택에 넣는다.
+    public static int solutino(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int answer = 0;
+        
+        for (char c : s.toCharArray()) {
+
+            if (Character.isDigit(c)) {
+                stack.push(Character.getNumericValue(c));
+            } else {
+                int num1 = stack.pop();
+                int num2 = stack.pop();
+
+                if (c == '+')
+                    stack.push(num2 + num1);
+                else if (c == '-')
+                    stack.push(num2 - num1);
+                else if (c == '*')
+                    stack.push(num2 * num1);
+                else
+                    stack.push(num2 / num1);
+            }
+        }
+
+        return stack.pop();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(solutino(br.readLine()));
+    }
+}
