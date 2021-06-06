b7b8d9dd5e3deb7356d5bd3970c3e2fc0c527a98
Haemin Jeong <jhm0828@gmail.com>
2021-06-06 20:54:18 +0900

    Create Main6_1.java

---
+++ inflearn/sorting_searching/Main6_1.java
@@ -0,0 +1,50 @@
+package inflearn.sorting_searching;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//선택정렬
+public class Main6_1 {
+
+    public static void solution(int[] arr, int n) {
+
+        for (int i = 0; i < n - 1; i++) {
+            int idx = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[idx] > arr[j]) {
+                    idx = j;
+                }
+            }
+
+            swap(arr, i, idx);
+        }
+    }
+
+    private static void swap(int[] arr, int n1, int n2) {
+        int temp = arr[n1];
+        arr[n1] = arr[n2];
+        arr[n2] = temp;
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; st.hasMoreTokens(); i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        solution(arr, n);
+
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
