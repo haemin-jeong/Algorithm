a9badb37edb28650716795228df2288318398b99
Haemin Jeong <jhm0828@gmail.com>
2021-06-14 20:54:42 +0900

    트리 구현시 공통으로 사용되는 Node 클래스 별도 분리 생성

---
+++ inflearn/recursive_tree_graph/Node.java
@@ -0,0 +1,13 @@
+package inflearn.recursive_tree_graph;
+
+public class Node {
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
