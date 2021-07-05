6abd0838f37cfa9d323a2fe18edeb340c3c5f2aa
Haemin Jeong <jhm0828@gmail.com>
2021-07-05 22:28:10 +0900

    lv1 폰켓몬 풀이 수정

--- programmers/level1/폰켓몬.java
+++ programmers/level1/폰켓몬.java
@@ -7,7 +7,6 @@ public class 폰켓몬 {
     public int solution(int[] nums) {
         Set<Integer> set = new HashSet<>();
         int len = nums.length;
-        int answer = 0;
 
         for(int i = 0; i < len; i++) {
             set.add(nums[i]);
