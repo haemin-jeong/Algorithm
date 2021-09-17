46a348205af4bd3a473c86131ba7517472ab4ac3
Haemin Jeong <jhm0828@gmail.com>
2021-09-17 15:00:17 +0900

    [백준] 5338 마이크로소프트 로고 풀이

---
+++ boj/boj_5338.java
@@ -0,0 +1,11 @@
+package boj;
+
+public class boj_5338 {
+    public static void main(String[] args) {
+        System.out.println("       _.-;;-._");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-;;-._|");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-''-._|");
+    }
+}
