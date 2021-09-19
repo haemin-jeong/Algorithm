fe40336da658354515071ed68086c75b176b4b36
Haemin Jeong <jhm0828@gmail.com>
2021-09-20 01:21:58 +0900

    [백준] 10818 최소,최대 재풀이

--- boj/boj_10818.java
+++ boj/boj_10818.java
@@ -6,26 +6,23 @@ package boj;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class boj_10818 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        String[] arr = br.readLine().split(" ");
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
 
-        int min = Integer.parseInt(arr[0]);
-        int max = Integer.parseInt(arr[0]);
-        for (int i = 1; i < n; i++) {
-            int num = Integer.parseInt(arr[i]);
-            if (max < num) {
-                max = num;
-                continue;
-            }
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
 
-            if (min > num)
-                min = num;
+            max = Math.max(max, num);
+            min = Math.min(min, num);
         }
 
-        System.out.printf("%d %d\n", min, max);
+        System.out.println(min + " " + max);
     }
 }
