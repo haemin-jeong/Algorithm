17fcd076486bd571a12570882af23027c18ba962
Haemin Jeong <jhm0828@gmail.com>
2021-09-25 23:08:51 +0900

    [백준] 2164 카드2 풀이

---
+++ boj/boj_2164.java
@@ -0,0 +1,29 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class boj_2164 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= n; i++) {
+            queue.offer(i);
+        }
+
+        while (true) {
+            if (queue.size() == 1) {
+                System.out.println(queue.poll());
+                return;
+            }
+
+            queue.poll();
+            queue.offer(queue.poll());
+        }
+    }
+}
