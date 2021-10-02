a448271ea6bb122266706fcc1cb9d79905a889e3
Haemin Jeong <jhm0828@gmail.com>
2021-10-02 10:44:52 +0900

    [백준] 11650 좌표 정렬하기 풀이

---
+++ boj/boj_11650.java
@@ -0,0 +1,53 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class boj_11650 {
+    static class Pos implements Comparable<Pos>{
+        int x;
+        int y;
+
+        public Pos(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Pos o) {
+            if (this.x == o.x) {
+                return this.y - o.y;
+            } else {
+                return this.x - o.x;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        Pos[] arr = new Pos[n];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            arr[i] = new Pos(x, y);
+        }
+
+        Arrays.sort(arr);
+
+
+        for (Pos pos : arr) {
+            sb.append(pos.x).append(" ").append(pos.y).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
