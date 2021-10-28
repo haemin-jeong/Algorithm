01521df21c69f5a1895b817766c2430dc181998a
haemin-jeong <jhm0828@gmail.com>
2021-10-28 20:56:33 +0900

    [프로그래머스] 나머지가 1이 되는 수 찾기 풀이

---
+++ programmers/level1/나머지가_1이_되는_수_찾기.java
@@ -0,0 +1,13 @@
+package programmers.level1;
+
+public class 나머지가_1이_되는_수_찾기 {
+    public int solution(int n) {
+        for(int i = 2; i < n; i++) {
+            if(n%i == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
