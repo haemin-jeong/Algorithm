39d2640ff314e554f226212c54b75ec2624e6063
Haemin Jeong <jhm0828@gmail.com>
2022-01-10 17:48:22 +0900

    [프로그래머스] 올바른 괄호 풀이

---
+++ programmers/level2/올바른_괄호.java
@@ -0,0 +1,23 @@
+package programmers.level2;
+
+import java.util.Stack;
+
+public class 올바른_괄호 {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack();
+
+        for(char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);
+            } else if(c == ')') {
+                if(stack.isEmpty()) {
+                    return false;
+                }
+
+                stack.pop();
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
