5d8bedf09feca6e996ff0110345a55e3db2d7c15
jeonghaemin <jhm0828@gmail.com>
2022-03-19 19:10:29 +0900

    [백준] N과 M(4) 풀이

---
+++ boj/boj_15652.java
@@ -0,0 +1,36 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_15652 {
+    static int[] combi;
+    static int N, M;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        combi = new int[M];
+        StringBuilder sb = new StringBuilder();
+        dfs(0,1, sb);
+        System.out.println(sb);
+    }
+
+    static void dfs(int l, int s, StringBuilder sb) {
+        if(l == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(combi[i]).append(" ");
+            }
+            sb.append("\n");
+        } else {
+            for (int i = s; i <= N; i++) {
+                combi[l] = i;
+                dfs(l+1, i, sb);
+            }
+        }
+    }
+}
