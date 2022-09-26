7cc092766d407bf552604659ebf777fd736111fa
jeonghaemin <jhm0828@gmail.com>
2022-09-26 15:24:29 +0900

    [백준] 1931 회의실 배정

---
+++ boj/boj_1931.java
@@ -0,0 +1,55 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class boj_1931 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        Meeting[] arr = new Meeting[n];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            arr[i] = new Meeting(start, end);
+        }
+
+        Arrays.sort(arr);
+
+        int answer = 0;
+        int end = 0;
+
+        for (Meeting m : arr) {
+            if (m.start >= end) {
+                end = m.end;
+                answer++;
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+    static class Meeting implements Comparable<Meeting> {
+        int start;
+        int end;
+
+        public Meeting(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        @Override
+        public int compareTo(Meeting o) {
+            if (this.end == o.end) {
+                return this.start - o.start;
+            }
+            return this.end - o.end;
+        }
+    }
+}
